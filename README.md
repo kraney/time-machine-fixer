000759c0c602298ab98fa6b82495cb15244b0095
Kris Raney <kraney@ixiacom.com>
2019-02-26 14:32:27 -0800

    Add some robustness and nicer output.

--- README.md
+++ README.md
@@ -13,3 +13,5 @@ because of an abrupt disconnect while a backup was running.
 The tools to repair this corruption are already in place on the system. This
 scripts makes use of them to repair the TM volume and get it working again. Then
 it runs a backup, which completes WITHOUT starting over.
+
+The output will look nicer if you have 'pv' installed. `brew install pv`
--- fix_timemachine.sh
+++ fix_timemachine.sh
@@ -33,66 +33,93 @@ DEST=afp://${USER}:${PASSWD}@${NAS}/$VOL
 ###
 function cleanup() {
 	if hdiutil info | grep image-path | grep /Volumes/$VOL > /dev/null; then
-		progress "Cleaning up TM attachment"
+		info "Cleaning up TM attachment"
 		hdiutil detach $DISK
 	fi
 	if [ -d /Volumes/$VOL ]; then
-		progress "Cleaning up TM mount"
+		info "Cleaning up TM mount"
 		umount /Volumes/$VOL
 		rm -fd /Volumes/$VOL
 	fi
-	progress "Re-enabling TM"
+	info "Re-enabling TM"
 	tmutil enable
 }
-trap cleanup SIGHUP SIGINT SIGTERM
+trap cleanup SIGHUP SIGINT SIGTERM ERR
 
 ###
 # Make sure messages are noticeable in to fsck output spam
 ###
-function progress() {
+function info() {
 	local msg="$1"
-	echo "###################################"
+	if ! which pv > /dev/null; then
+		echo "###################################"
+	fi
 	echo $msg
-	echo "###################################"
+	if ! which pv > /dev/null; then
+		echo "###################################"
+	fi
 }
 
-progress "Temporarily disabling Time Machine"
+function progress_bar() {
+	local funcname="$1"
+	local lines="$2"
+	local flags="$3"
+	if which pv > /dev/null; then
+		pv -l "$flags" -N "$funcname" -s "$lines"
+	else
+		cat
+	fi
+}
+
+function attach() {
+	local bundle=$1
+	sync
+	DISK=$(hdiutil attach -nomount -readwrite -noverify -noautofsck $bundle | grep HFS | awk '{ print $1 }')
+	if [ x"$DISK" == x ]; then
+		# Didn't give us a disk name. Must be "temporarily unavailable", i.e.
+		# already attached.
+		sleep 1
+		DISK=$(hdiutil attach -nomount -readwrite -noverify -noautofsck $bundle | grep HFS | awk '{ print $1 }')
+	fi
+}
+
+info "Temporarily disabling Time Machine"
 tmutil disable
 
-progress "Mounting Time Machine NAS volume"
+info "Mounting Time Machine NAS volume"
 mkdir -p /Volumes/$VOL
-if ! mount | grep /Volumes/$VOL; then
+if ! mount | grep /Volumes/$VOL > /dev/null; then
 	mount_afp $DEST /Volumes/$VOL
 fi
 
-progress "Setting permissions"
-chflags -R nouchg /Volumes/$VOL/$HOSTNAME.sparsebundle
+info "Setting permissions"
+chflags -R nouchg /Volumes/$VOL/$HOSTNAME.sparsebundle | progress_bar chflags 1 -t
 
-progress "Attaching time machine sparsebundle"
-DISK=$(hdiutil attach -nomount -readwrite -noverify -noautofsck /Volumes/$VOL/$HOSTNAME.sparsebundle | grep HFS | awk '{ print $1 }')
-progress "Doing fsck -p. This isn't always necessary, but can help in some troublesome cases"
-fsck_hfs -p $DISK
-progress "Detaching time machine sparsebundle"
+info "Attaching time machine sparsebundle"
+attach /Volumes/$VOL/$HOSTNAME.sparsebundle
+info "Doing fsck -p. This isn't always necessary, but can help in some troublesome cases"
+fsck_hfs -pfy $DISK | progress_bar "fsck_hfs -p" 40 -tep
+info "Detaching time machine sparsebundle"
 hdiutil detach $DISK
 
-progress "Re-attaching time machine sparsebundle"
-DISK=$(hdiutil attach -nomount -readwrite -noverify -noautofsck /Volumes/$VOL/$HOSTNAME.sparsebundle | grep HFS | awk '{ print $1 }')
-progress "Doing fsck -drfy. This is what directly addresses the issue."
-fsck_hfs -drfy $DISK
-progress "Detaching time machine sparsebundle"
+info "Re-attaching time machine sparsebundle"
+attach /Volumes/$VOL/$HOSTNAME.sparsebundle
+info "Doing fsck -drfy. This is what directly addresses the issue."
+fsck_hfs -drfy $DISK | progress_bar "fsck_hfs -drfy" 40 -tep
+info "Detaching time machine sparsebundle"
 hdiutil detach $DISK
 
-progress "Updating plist to remove failed state, so TM will try again"
+info "Updating plist to remove failed state, so TM will try again"
 plutil -remove RecoveryBackupDeclinedDate /Volumes/$VOL/$HOSTNAME.sparsebundle/com.apple.TimeMachine.MachineID.plist
 plutil -replace VerificationState -integer 0 /Volumes/$VOL/$HOSTNAME.sparsebundle/com.apple.TimeMachine.MachineID.plist
-progress "Unmounting Time Machine NAS volume"
+info "Unmounting Time Machine NAS volume"
 umount /Volumes/$VOL
 rm -fd /Volumes/$VOL
 
-progress "Re-enabling Time Machine"
+info "Re-enabling Time Machine"
 tmutil enable
 
 trap - SIGHUP SIGINT SIGTERM
 
-progress "Starting a backup"
+info "Starting a backup"
 tmutil startbackup
